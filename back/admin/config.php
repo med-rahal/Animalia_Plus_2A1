a2dd6cf84e1cc79e6e1df9e07ac0818552f97715
raed guembri <60182483+raed717@users.noreply.github.com>
2021-05-06 20:55:51 +0100

    Update config.php

--- back/admin/config.php
+++ back/admin/config.php
@@ -7,7 +7,7 @@
     {
       if (!isset(self::$pdo)) {
         try{
-          self::$pdo = new PDO('mysql:host=localhost;dbname=animalia_raed', 'root','',
+          self::$pdo = new PDO('mysql:host=localhost;dbname=animalia', 'root','',
           [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
